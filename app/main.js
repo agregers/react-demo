9afba3c52426bb55c9332920f312d31701176e66
Alexis Gregerson <agregers@uw.edu>
2016-02-08 16:35:58 -0800

    added pic of puppy

--- app/main.js
+++ app/main.js
@@ -5,14 +5,20 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 //message to show
-var message = React.createElement(
-    'h1',                  //html tag
-    {attribute : 'value'}, //object of attributes
-    'Hello World!!'         //content
-); 
+// var message = React.createElement(
+//     'h1',                  //html tag
+//     {attribute : 'value'}, //object of attributes
+//     'Hello World!!'         //content
+// ); 
 
+//message to show using JSX (embedded html)
+var message = <h1 id="title">Hello World!</h1>
+
+var imgUrl = 'https://campuswellbeing.files.wordpress.com/2011/10/cute-puppies-with-worms.jpg';
+var pic = <img src = {imgUrl} />
 
 //show the content in the web page (inside the tag with id="content")
-ReactDOM.render(message, document.getElementById('content'));
+//ReactDOM.render(message, document.getElementById('content'));
+ReactDOM.render(pic, document.getElementById('content'));
 
-console.log('message rendered!');
+console.log('pic rendered!');
